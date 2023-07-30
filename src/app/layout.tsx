e9125ad399d4795fa4860a8013240a14b99436fb
Rakshit Pathak <rajatpathak435@gmail.com>
2023-07-30 19:28:10 +0530

    changed layout to server component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,7 @@
-"use client";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Heart, Menu, X } from "lucide-react";
-import Sidebar from "@/components/Sidebar";
-import { useState } from "react";
-import Link from "next/link";
-import Footer from "@/components/Footer";
-import Search from "@/components/SearchInput";
-import SearchInput from "@/components/SearchInput";
+import SidebarLayout from "@/components/SidebarLayout";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,42 +14,10 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const [isNavOpen, setIsNavOpen] = useState(false);
-
-  const toggleNav = () => {
-    setIsNavOpen((prev) => !prev);
-  };
-
   return (
     <html lang="en">
       <body className={`${inter.className} text-gray-300  w-screen`}>
-        <Sidebar isNavOpen={isNavOpen} toggleNav={toggleNav} />
-        <div className="bg-darkBlue md:pl-[24rem] mx-auto md:w-full h-screen overflow-auto ">
-          <div className="w-11/12 mx-auto h-full flex flex-col max-w-screen-xl">
-            <div className="flex-grow shrink-0 basis-auto">
-              <nav className="h-24 block gap-4 flex justify-between w-full items-center">
-                <Link
-                  href="/search/hey"
-                  className="flex hover:text-white items-center"
-                >
-                  <Heart />{" "}
-                  <span className="ml-2 hidden md:block text-lg">
-                    My Favorites
-                  </span>
-                </Link>
-                <SearchInput />
-                <button
-                  className=" md:hidden flex items-center text-blue-500"
-                  onClick={toggleNav}
-                >
-                  {isNavOpen ? <X /> : <Menu />}
-                </button>
-              </nav>
-              <main className=" w-11/12 mx-auto md:mx-0 my-6">{children}</main>
-            </div>
-            <Footer />
-          </div>
-        </div>
+        <SidebarLayout>{children}</SidebarLayout>
       </body>
     </html>
   );
---
+++ src/components/SidebarLayout/index.tsx
@@ -0,0 +1,49 @@
+"use client";
+import React, { useState } from "react";
+import Sidebar from "../Sidebar";
+import Link from "next/link";
+import { Heart, Menu, X } from "lucide-react";
+import SearchInput from "../SearchInput";
+import Footer from "../Footer";
+
+const SidebarLayout = ({ children }: { children: React.ReactNode }) => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavOpen((prev) => !prev);
+  };
+
+  return (
+    <>
+      <Sidebar isNavOpen={isNavOpen} toggleNav={toggleNav} />
+      <div className="bg-darkBlue md:pl-[24rem] mx-auto md:w-full h-screen overflow-auto ">
+        <div className="w-11/12 mx-auto h-full flex flex-col max-w-screen-xl">
+          <div className="flex-grow shrink-0 basis-auto">
+            <nav className="h-24 block gap-4 flex justify-between w-full items-center">
+              <Link
+                href="/search/hey"
+                className="flex hover:text-white items-center"
+              >
+                <Heart />{" "}
+                <span className="ml-2 hidden md:block text-lg">
+                  My Favorites
+                </span>
+              </Link>
+              <SearchInput />
+              <button
+                className=" md:hidden flex items-center text-blue-500"
+                onClick={toggleNav}
+              >
+                {isNavOpen ? <X /> : <Menu />}
+              </button>
+            </nav>
+            <main className=" w-11/12 mx-auto md:mx-0 my-6">{children}</main>
+          </div>
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SidebarLayout;
